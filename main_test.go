e82fb077706a0c95623a117041ec57a1a0dd2648
Mohammad Khafid Barokum <navigator619@gmail.com>
2022-03-23 22:41:00 +0700

    complete test and benchmark

--- main_test.go
+++ main_test.go
@@ -6,8 +6,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
-	main()
+func TestUseChannel(t *testing.T) {
+	assert.Equal(t, concurrent*totalExecution, UseChannel())
+}
 
-	assert.Equal(t, concurrent*2, deposit)
+func TestUseMutex(t *testing.T) {
+	assert.Equal(t, concurrent*totalExecution, UseMutex())
+}
+
+func TestSynchronous(t *testing.T) {
+	assert.Equal(t, concurrent*totalExecution, Synchronous())
+}
+
+func TestUseArray(t *testing.T) {
+	assert.Equal(t, concurrent*totalExecution, UseArray())
+}
+
+func BenchmarkGoroutine(b *testing.B) {
+	b.Run("synchronous", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			Synchronous()
+		}
+	})
+
+	b.Run("use mutex", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			UseMutex()
+		}
+	})
+
+	b.Run("use channel", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			UseChannel()
+		}
+	})
+
+	b.Run("use array", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			UseChannel()
+		}
+	})
 }
